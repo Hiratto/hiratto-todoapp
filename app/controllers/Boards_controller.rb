656cd9fcaa9a0b0c7916e3ade2d322691a8c64dd
TAKAHISA HIRANO <hiratto@me.com>
2021-11-25 20:57:47 +0900

    boardとuserを紐付け

--- app/controllers/Boards_controller.rb
+++ app/controllers/Boards_controller.rb
@@ -4,6 +4,16 @@ class BoardsController < ApplicationController
   end
 
   def new
-    @boards = boards.new
+    @boards = Boards.new
   end
+
+  def create
+    @boards = Boards.create(board_params)
+    redirect_to board_path
+  end
+
+  private
+    def board_params
+      params.require(:board).permit(:name)
+    end
 end
